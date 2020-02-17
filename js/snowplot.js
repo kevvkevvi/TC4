54eaa8115dae0a39a5af5eec4d2b5056711c12b1
kevvkevvi <54913154+kevvkevvi@users.noreply.github.com>
2020-02-17 12:12:53 -0500

    color change

--- js/snowplot.js
+++ js/snowplot.js
@@ -5,5 +5,5 @@ canvas.height = 300;
 // Grab the context of the canvas so that we can draw.
 var ctx = canvas.getContext("2d");
 // Change the background color of the canvas.
-ctx.fillStyle = "snow";
+ctx.fillStyle = "lime";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
